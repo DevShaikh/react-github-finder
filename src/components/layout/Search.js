38f14e062c3dbc5ab6dde5c1ee7e1f6e51769f5a
N2SPARTAN <asofficial4k@gmail.com>
2020-09-23 02:43:49 +0500

    React App

---
+++ src/components/layout/Search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Search = () => {
+  return (
+    <div className="container">
+      <div className="navbar navbar-light bg-dark">
+        <h2>Github Finder</h2>
+      </div>
+    </div>
+  );
+};
+export default Search;
